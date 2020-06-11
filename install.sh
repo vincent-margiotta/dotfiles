3823dd5ab17775fda58053809634281abb9ee0b9
Vincent J. Margiotta <vmargiotta@iseatz.com>
2020-06-11 15:16:41 -0500

    clean up docker functions

--- install.sh
+++ install.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
 function __stow() {
-    $(which stow) --verbose=1 \
-	    	  --target="$HOME" \
-		  --dotfiles \
-		  --restow "$1"
+    $(which stow) --verbose=1 --target="$HOME" --dotfiles --restow "$1"
 }
 
-__stow alacritty
-__stow aliases
-__stow chunkwm
+# __stow alacritty
+# __stow aliases
+# __stow chunkwm
 __stow functions
-__stow git
-__stow oh-my-zsh
-__stow skhd
-__stow tmux
-__stow vim
-__stow zsh
+# __stow git
+# __stow oh-my-zsh
+# __stow skhd
+# __stow tmux
+# __stow vim
+# __stow zsh
