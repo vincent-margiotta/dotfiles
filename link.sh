1c56c99fbd96e78cf0390b66a971cc1c8390e2ba
Vincent J. Margiotta <vincent.joseph.margiotta.jr@gmail.com>
2021-03-20 13:15:00 -0400

    clean up link implementation

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#local !/usr/bin/env bash
 
 ##
 # Non-sophisticated dotfile management.
@@ -7,31 +7,31 @@
 # preserving its file type. The top-most directory used by this repository is
 # removed before linkage.
 #
-# For example, the path `foo/dot-foo/config/dot-barrc` becomes
-# `.foo/config/.barrc` after linkage.
+# For example, the path `foo/dot-foo/config/dot-foorc` becomes
+# `.foo/config/.foorc` after linkage.
 
-# Install dotfiles into home directory.
-# TODO: Make this configurable.
-TARGET="$HOME"
+if [ -z "$DOTFILE_TARGET" ]; then
+  # Default to installing into $HOME if an override is unset.
+  TARGET="$HOME"
+else
+  TARGET="$DOTFILE_TARGET"
+fi
 
-# It's unlikely `ln` won't be installed, but check anyway.
+# Brief sanity check that the `ln` binary exists.
 LINKER="$(command -v ln)"
 [ "$?" -eq 0 ] || { echo "No supported linker found! "; exit 1; }
 
-# This works on individual dotfiles.
-[ "$#" -gt 0 ] || { echo "Supply a dotfile. "; exit 2; }
+[ "$#" -gt 0 ] || { echo "Supply a dotfile. "; exit 2; }     # This works on individual dotfiles.
+[ -d "$1" ] && { echo "Cannot link directories. "; exit 2; } # Abort if the dotfile is just a directory.
 
-# Abort if the dotfile is just a directory.
-[ -d "$1" ] && { echo "Cannot link directories. "; exit 2; }
+# The Magic Sauce.
+DOTFILE="$(echo "$1" | cut -d '/' -f2- | sed 's/dot-/./g')"
 
-# Convert the `dot-` prefix to its hidden form & remove the top-most directory.
-DOTFILE="$(echo "$1" | sed 's/dot-/./g' | cut -d '/' -f2-)"
-
-# Create the parent directory, if it does not exist.
 PARENT="$(dirname $TARGET/$DOTFILE)"
-[ -d "$PARENT" ] || { echo "Creating parent directory. "; mkdir -p "$PARENT"; }
+# Create the parent directory, if it does not exist.
+[ -d "$PARENT" ] || { mkdir -p "$PARENT"; echo "Created parent $PARENT"; }
 
 # Create symbolic links, overwriting any existing files.
-echo Linking "$1" as "$TARGET/$DOTFILE"...
-"$LINKER" -fs "$PWD/$1" "$TARGET/$DOTFILE"
+FULLPATH="$TARGET/$DOTFILE"
+"$LINKER" -fs "$PWD/$1" "$FULLPATH" && echo "Linked $1 to $FULLPATH" && exit 0
 
