f984a5939470cda064d6367addd4113ac826b47d
Vincent J. Margiotta Jr <vincent.joseph.margiotta.jr@gmail.com>
2022-04-10 11:34:39 -0400

    refactor linker to be clearer

--- link.sh
+++ link.sh
@@ -1,28 +1,25 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 ##
-# Non-sophisticated dotfile management.
+# Simple dotfile management.
 #
-# Transform any path elements prefixed with `dot-` to their hidden form.
-# The top-most directory is removed before linkage.
+# Apply two rules to files before creating the symlink:
+# 1. Top-level directory names are ignored
+# 2. All instances of `dot-` are converted to `.`
 #
-# For example, the path `foo/dot-foo/config/dot-foorc` becomes
-# `.foo/config/.foorc` after linkage.
-
-# Error cases
-[ "$#" -gt 0 ] || { echo "Supply a dotfile. "; exit 2; }
-[ -f "$1" ] || { echo "Can only link regular files. "; exit 2; }
+# For example, the dotfile `bash/dot-bashrc` becomes `~/.bashrc`.
 
 TARGET="$HOME"  # Default to installing into $HOME unless an override is set.
 [ -n "$DOTFILES" ] && TARGET="$DOTFILES"
-[ -d "$TARGET" ] || { echo "TARGET is not a directory. "; exit 1; }
 
-# Brief sanity check that the `ln` binary exists.
-LINKER="$(command -v ln)" || { echo "No supported linker found! "; exit 1; }
+# Handle error cases
+[ "$#" -gt 0 ]            || { echo "You must supply a dotfile. ";   exit 2; }
+[ -f "$1" ]               || { echo "Can only link regular files. "; exit 2; }
+[ -d "$TARGET" ]          || { echo "TARGET is not a directory. ";   exit 1; }
+LINKER="$(command -v ln)" || { echo "No supported linker found! ";   exit 1; }
 
-# The Magic Sauce.
+# The Magic Sauce
 DOTFILE="$(echo "$1" | cut -d '/' -f2- | sed 's/dot-/./g')"
-
 FULLPATH="$TARGET/$DOTFILE"
 PARENT="$(dirname "$FULLPATH")"
 
@@ -30,4 +27,3 @@ PARENT="$(dirname "$FULLPATH")"
 
 # Echo and return exit code 0 only if the link succeeds.
 "$LINKER" -fs "$PWD/$1" "$FULLPATH" && echo "Linked $1 to $FULLPATH" && exit 0
-
